35b0ee407a913a24f7418d712acbb5062c8521e7
zahraahmadi2025 <zahraahmadi.43556@gmail.com>
2025-04-07 06:24:57 +0430

    js added

--- public/login-page1.js
+++ public/login-page1.js
@@ -3,15 +3,15 @@ const bars = document.getElementById("bar");
 const vnav =document.getElementById("vnav");
 const hnav =document.getElementById("hnav");
 bars.addEventListener("click",()=>{
-    hnav.classList.add("w-[80%]");
+  if(hnav.classList.add("w-[80%]"));
  hnav.classList.remove("w-full");
  vnav.classList.remove("hidden");
- bars.style.display("block");
+vnav.classList.add("flex");
  
 });
-bars.addEventListener("click",()=>{
- hnav.classList.add("w-full");
- hnav.classList.remove("w-[80%]");
- vnav.classList.remove("hidden");
+// bars.addEventListener("click",()=>{
+//  hnav.classList.add("w-full");
+//  hnav.classList.remove("w-[80%]");
+//  vnav.classList.remove("hidden");
 
-});
+// });
